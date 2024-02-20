0c5e63e30e4c69a3ffbb2103fdfbd09d7b15fef9
ABAnshu03 <160565516+ABAnshu03@users.noreply.github.com>
2024-02-21 02:25:00 +0530

    updated

--- 2insertiondec.c
+++ 2insertiondec.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-
-void incodr(int n[], int length) {
+void decodr(int n[], int length) {
     for (int i = 1; i < length; i++) {
         int key = n[i];
         int j = i - 1;
-        while (j >= 0 && n[j] > key) {
+        while (j >= 0 && n[j] < key) {
             n[j + 1] = n[j];
             j = j - 1;
         }
         n[j + 1] = key;
     }
 }
-
 int main() {
     printf("Insertion sorting\n");
     int num[] = {5, 3, 1, 4, 1, 2};
     int length = sizeof(num) / sizeof(num[0]);
 
-    printf("Increasing Order:\n");
-    incodr(num, length);
+    printf("\nDecreasing Order:\n");
+    decodr(num, length);
     for (int i = 0; i < length; i++) {
         printf("%d ", num[i]);
     }
